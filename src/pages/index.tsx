5860e8ae0a0a1dad37aab4383c47dc6677021cdb
John Meguerian <johnmeguerian@gmail.com>
2020-05-14 11:17:51 -0400

    match order of homepage links to nav links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,11 @@ import Layout from "../components/Layout"
 const IndexPage = () => (
   <Layout>
     <div className="Grid HomePage">
-      <Link href="/easing">
-        <a className="HomePage-link">Easing Curves</a>
-      </Link>
       <Link href="/ratio">
         <a className="HomePage-link">Aspect Ratios</a>
+      </Link>
+      <Link href="/easing">
+        <a className="HomePage-link">Easing Curves</a>
       </Link>
       <Link href="/letterspacer">
         <a className="HomePage-link">Letterspacing</a>
